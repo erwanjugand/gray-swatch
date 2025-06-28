9df9b19d76d27585bb155685d71e6113957f5cef
Erwan Jugand <erwan.jugand@gmail.com>
2025-06-28 17:32:33 +0200

    chore(eslint): move conf to ts + clean

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,13 @@
 import eslint from '@eslint/js'
 import tsConfig from 'typescript-eslint'
-import prettierConfig from 'eslint-config-prettier'
+import prettierConfig from 'eslint-config-prettier/flat'
 import vueConfig from 'eslint-plugin-vue'
-import vueParser from 'vue-eslint-parser'
 import globals from 'globals'
 
 export default tsConfig.config(
   eslint.configs.recommended,
-  ...vueConfig.configs['flat/recommended'],
   ...tsConfig.configs.strict,
+  ...vueConfig.configs['flat/recommended'],
   prettierConfig,
   {
     rules: {
@@ -21,7 +20,6 @@ export default tsConfig.config(
     },
     languageOptions: {
       globals: globals.browser,
-      parser: vueParser,
       parserOptions: {
         parser: tsConfig.parser,
       },
