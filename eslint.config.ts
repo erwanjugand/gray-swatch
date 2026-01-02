f8e8cae756a20b3a109b85a46d3794217cd6c0d3
Erwan Jugand <erwan.jugand@gmail.com>
2026-01-02 22:07:22 +0100

    chore(eslint): migrate custom vue rules to packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,31 +1,7 @@
-import eslint from '@eslint/js'
-import tsConfig from 'typescript-eslint'
 import prettierConfig from 'eslint-config-prettier/flat'
-import vueConfig from 'eslint-plugin-vue'
-import globals from 'globals'
+import { baseConfig, vueConfig } from '@erwanjugand/eslint-config'
+import { defineConfig } from 'eslint/config'
 
-export default tsConfig.config(
-  eslint.configs.recommended,
-  ...tsConfig.configs.strict,
-  ...vueConfig.configs['flat/recommended'],
-  prettierConfig,
-  {
-    rules: {
-      'vue/block-order': [
-        'error',
-        {
-          order: ['script', 'template', 'style'],
-        },
-      ],
-    },
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        parser: tsConfig.parser,
-      },
-    },
-  },
-  {
-    ignores: ['dist'],
-  },
-)
+export default defineConfig(baseConfig, vueConfig, prettierConfig, {
+  ignores: ['dist'],
+})
